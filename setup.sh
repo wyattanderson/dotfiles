7a51f31ebae191cb1409b0ec9f1c3e893cca03bd
Wyatt Anderson <wanderson@gmail.com>
2011-09-04 18:20:55 -0400

    Fixing setup script.

--- setup.sh
+++ setup.sh
@@ -7,14 +7,14 @@ if [ -e ~/.bash_profile ] && [ ! -h ~/.bash_profile ]; then
    read -p "Move existing .bash_profile to .bash_profile_old? " -n 1
    if [[ $REPLY =~ ^[Yy]$ ]]; then
       mv ~/.bash_profile ~/.bash_profile_old
-      ln -s ~/dotfiles/bash_profile ~/.bash_profile
    fi
 fi
+ln -s ~/dotfiles/bash_profile ~/.bash_profile
 
 if [ -e ~/.bashrc ] && [ ! -h ~/.bashrc ]; then
    read -p "Move existing .bashrc to .bashrc_old? " -n 1
    if [[ $REPLY =~ ^[Yy]$ ]]; then
       mv ~/.bashrc ~/.bashrc_old
-      ln -s ~/dotfiles/bashrc ~/.bashrc
    fi
 fi
+ln -s ~/dotfiles/bashrc ~/.bashrc
