2ef9e04c1cf80fdb34fc6be5dc0b251c16346f01
Wyatt Anderson <wanderson@gmail.com>
2025-03-16 10:35:39 -0700

    fixes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,11 +1,5 @@
 vim.g.mapleader = ","
 
+require("config.options")
+require("config.keymaps")
 require("config.lazy")
-
-vim.api.nvim_create_autocmd("User", {
-  pattern = "VeryLazy",
-  callback = function()
-    require("config.keymaps")
-    require("config.options")
-  end,
-})
