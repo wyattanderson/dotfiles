6d80fa4980dc42dc1f1ae5fb36576a714b72d8d4
Wyatt Anderson <wanderson@gmail.com>
2025-03-20 13:16:36 -0700

    wip

---
+++ .config/nvim/ftdetect/sls.vim
@@ -0,0 +1,2 @@
+au BufRead,BufNewFile *.sls     set ft=sls
+au FileType sls                 set ts=2 ts=2 sts=2
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,6 +2,10 @@ return {
   "nvim-telescope/telescope.nvim",
   dependencies = {
     "nvim-lua/plenary.nvim",
+    {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
+    },
   },
   keys = {
     { "<c-p>", "<cmd>Telescope find_files<cr>" },
